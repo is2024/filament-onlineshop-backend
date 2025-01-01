308f3e94e38184ec33a1037b1b623f93c7d48295
is2024 <is.mohidin@gmail.com>
2025-01-01 15:41:20 +0800

    git init orders-order detail

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\StoreShow;
 use App\Livewire\ProductDetail;
 use App\Livewire\ShoppingCart;
+use App\Livewire\Order;
+use App\Livewire\OrderDetail;
+
 
 Route::get('/', StoreShow::class)->name('home');
 Route::get('/product', ProductDetail::class)->name('product.detail');
 Route::get('/shopping-cart', ShoppingCart::class)->name('shopping-cart');
+Route::get('/orders', Order::class)->name('orders');
+Route::get('/order-detail', OrderDetail::class)->name('order-detail');
