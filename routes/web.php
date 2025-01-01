642c32ec9e5acc3e8b1b2573c95ff5c8ed6848b1
is2024 <is.mohidin@gmail.com>
2025-01-01 15:16:14 +0800

    git init product detail-shopping chart

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\StoreShow;
+use App\Livewire\ProductDetail;
+use App\Livewire\ShoppingCart;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', StoreShow::class)->name('home');
+Route::get('/product', ProductDetail::class)->name('product.detail');
+Route::get('/shopping-cart', ShoppingCart::class)->name('shopping-cart');
